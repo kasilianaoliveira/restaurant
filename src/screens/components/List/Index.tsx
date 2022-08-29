70004d511c5010613afdb6f209d987b14fe68f78
kasilianaoliveira <kasilianaoliveira@gmail.com>
2022-08-29 00:34:57 -0300

    Add category list

---
+++ src/screens/components/List/Index.tsx
@@ -0,0 +1,22 @@
+import "./style.scss";
+
+interface Props {
+  id: string;
+  isActive: boolean;
+  icon: string;
+  label: string;
+  onClick: (id: string) => void;
+}
+export const List = ({ id, isActive, icon, label, onClick }: Props) => {
+  return (
+    <li
+      id={id}
+      key={id}
+      onClick={() => onClick(id)}
+      className={isActive ? "list-container selected" : "list-container"}
+    >
+      <img className="color-select" src={icon} alt={label} />
+      <p>{label}</p>
+    </li>
+  );
+};
